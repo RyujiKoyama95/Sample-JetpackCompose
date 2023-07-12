b3f4cc0c11cc56d0979259a54e67cf7c68ea5ed5
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-12 09:20:39 +0900

    Add Counter component

--- app/src/main/java/com/example/samplejetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/samplejetpackcompose/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.samplejetpackcompose.ui.theme.SampleJetpackComposeTheme
@@ -19,10 +22,9 @@ class MainActivity : ComponentActivity() {
             SampleJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Counter()
                 }
             }
         }
@@ -30,17 +32,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleJetpackComposeTheme {
-        Greeting("Android")
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+    Button(onClick = { count.value++ }) {
+        Text(text = "Count:${count.value}")
     }
 }
